06817bd2072d02a23ee723f251abcac4dcd51074
KENDRA GO <kendra.go@obf.ateneo.edu>
2021-03-25 17:33:00 +0800

    name is asked only once now. edited code to follow pep8 and style guides.

--- bujo_app/views.py
+++ bujo_app/views.py
@@ -3,27 +3,39 @@ from django.http import HttpResponse
 
 from .forms import UsernameForm
 
+asked_name = False
+name = None
+
 
 def home(request):
-    if request.method == 'POST':
+    global asked_name, name
+
+    if asked_name:
+        return render(request, "home.html", {'output': name})
+    elif request.method == 'POST':
         form = UsernameForm(request.POST)
         if form.is_valid():
+            asked_name = True
             name = form.cleaned_data['name']
             return render(request, "home.html", {'output': name})
-        else:
-            render(request, "home.html", {'form': form})
-    else:
-        form = UsernameForm()
+
+        render(request, "home.html", {'form': form})
+
+    form = UsernameForm()
     return render(request, "home.html", {'form': form})
+
 
 def profile(request):
     return render(request, 'profile.html')
 
+
 def key(request):
     return render(request, 'key.html')
 
+
 def this_week(request):
     return render(request, 'this_week.html')
+
 
 def today(request):
     return render(request, 'today.html')
--- bujo_project/settings.py
+++ bujo_project/settings.py
@@ -126,5 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bujo_project/static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bujo_project/static'), ]
